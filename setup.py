422a06e7b3cca90c362cb3f00d30141cb7a5330a
Ben Jaderberg <benjamin.jaderberg@physics.ox.ac.uk>
2021-06-08 14:18:16 +0100

    Add setup.py for easy pip installation.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quantum-neural-network',
+    version='0.1.0',
+    author='Benjamin Jaderberg',
+    author_email='benjamin.jaderberg@physics.ox.ac.uk',
+    packages=find_packages(),
+    scripts=[],
+    url='https://github.com/bjader/quantum-neural-network',
+    license='LICENSE',
+    description='For building quantum neural networks in Qiskit and integrating with PyTorch',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "qiskit",
+        "torch",
+        "torchvision",
+        "torchsummary"
+    ],
+)
